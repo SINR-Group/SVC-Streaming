0a6dbc7c6bf2cfb2e13c97cd40b1636fc6a19eb3
adeep24 <adeep@cs.stonybrook.edu>
2020-06-18 00:36:00 -0400

    network simulation and json build scripts

---
+++ client/utils/video_JSON_build.py
@@ -0,0 +1,36 @@
+import os
+import json
+
+VIDEO_PATH = '/home/arian/aakash/10minVid/static/video1'
+TOTAL_SEGMENTS = 300
+TOTAL_BITRATES = 5
+
+properties = {}
+properties['total_segments'] = TOTAL_SEGMENTS
+properties['total_representations'] = TOTAL_BITRATES
+properties['bitrates'] = [214915, 562660, 990946, 1520727, 2963872] #bits per second
+properties['total_duration'] = 600 #seconds
+
+# media file details such as file name, segment duration, start index of segments
+# change $REPID$ with representation ID and $Number$ with segment number to download.
+properties['media'] = 'video_$REPID$_dash$Number$.zip'
+properties['duration'] = 2000
+properties['timescale'] = 1000
+properties['start_number'] = 0
+
+# video segment sizes
+segments_size = []
+for i in range(0,TOTAL_SEGMENTS):
+    seg_size = []
+    for j in range(TOTAL_BITRATES):
+        fp = VIDEO_PATH + '/' + 'video_$q$_dash$n$.zip'.replace('$q$',str(j+1)).replace('$n$',str(i))
+        s = os.path.getsize(fp)
+        seg_size.append(s)
+    
+    segments_size.append(seg_size)
+properties['segment_size_bytes'] = segments_size
+
+# writes dict to file
+with open('video_properties.json','w') as f :
+    json.dump(properties, f)
+
